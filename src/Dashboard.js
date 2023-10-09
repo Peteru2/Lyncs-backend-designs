3ac2bfeca4f4fb199810c066afc353f6737eb6b2
DevPeter <polalekan526@gmail.com>
2023-10-09 16:55:18 +0100

    Work in progress

---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+    return ( 
+        <>
+            <section>
+                
+            </section>
+        </>
+     );
+}
+ 
+export default Dashboard;
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,23 +1,54 @@
 import loggo from  "./images/logo.png"
+import DashIcon1 from "./images/category.svg"
+import DashIcon2 from "./images/element-3.svg"
+import PendOIcon1 from "./images/ShoppingCartSimple2.svg"
+import PendOIcon2 from "./images/ShoppingCartSimple.svg"
+import PendDIcon1 from "./images/Truck2.svg"
+import PendDIcon2 from "./images/Truck.svg"
+import AwaitIcon1 from "./images/Alarm2.svg"
+import AwaitIcon2 from "./images/Alarm.svg"
+import ClosedIcon1 from "./images/CalendarX2.svg"
+import ClosedIcon2 from "./images/CalendarX.svg"
+
 import { Link, useLocation } from "react-router-dom";
 const SideBar = () => {
     const navSideList = [
         {
             title: "Dashboard",
-            url: "/"
+            url: "/",
+            icon1: DashIcon1,
+            icon2: DashIcon2
         },
         {
             title: "Pending Orders",
-            url: ""
+            url: "",
+            icon1: PendOIcon1,
+            icon2: PendOIcon2
+
 
         },
         {
             title: "Pending Deliveries",
-            url: ""
+            url: "",
+            icon1: PendDIcon1,
+            icon2: PendDIcon2
+
+
+        },
+        {
+            title: "Awaiting Feedbacks",
+            url: "",
+            icon1: AwaitIcon1,
+            icon2: AwaitIcon2
+
         },
         {
             title: "Closed Deliveries",
-            url: ""
+            url: "",
+            icon1: ClosedIcon1,
+            icon2: ClosedIcon2
+
+
         },
 
 
@@ -26,17 +57,17 @@ const SideBar = () => {
     const location = useLocation()
     return ( 
         <>
-            <aside className="bg-white sideBar border-r-2 w-48">
+            <aside className="bg-white sideBar border-r-2 w-72">
                     <div className="p-4  border-b-2 flex h-16 justify-center">
                         <img src={loggo} alt="logo" />
                     </div>
 
             <div>
-                <div className=" text-gray-500 mt-12">
+                <div className=" text-gray-500 mt-12 ">
                     {navSideList.map((item,index) =>{
                         return(
-                    <div key={index} className={`flex text-sm my-8 cursor-pointer ${location.pathname === item.url ? "border-r-2 border-yellow-200" :""} `}>
-                      <Link to={item.url} className="flex">  <i className="fa fa-dashbord mr-5 ">d</i><p>{item.title}</p></Link>
+                    <div key={index} className={`flex text-sm pl-6 my-6 py-2 cursor-pointer  ${location.pathname === item.url ? "Sidebar-border" :""} `}>
+                      <Link to={item.url} className="flex items-center">  { location.pathname=== item.url ? (<img src={item.icon1} alt="l" className="fa fa-dashbord mr-5 "/>) :(<img src={item.icon2} alt="l" className="fa fa-dashbord mr-5 "/>)}<p className={location.pathname === item.url ? "text_color" :""}>{item.title}</p></Link>
                     </div>
 
                     )
@@ -44,8 +75,8 @@ const SideBar = () => {
                 }
                 </div>
             </div>
-            <div className="text-center text-gray-500 mb-auto">
-
+            <div className="text-center text-gray-500 mt-auto">
+                   
              <button>
                <i className="fa fa-logout"></i> Logout
              </button>
